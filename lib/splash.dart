279a29483fb86fcb903ad9ade62e2a76b7bef26d
Sparsh2509 <sparshgupta250904@gmail.com>
2024-10-23 23:41:03 +0530

    Splash screen created

--- lib/splash.dart
+++ lib/splash.dart
@@ -14,54 +14,22 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState() {
-    _goToMenuPage();
-    super.initState();
-  }
-
-  _goToMenuPage() {
-    Future.delayed(const Duration(seconds: 2)).then((_) {
-      Navigator.of(context).pushAndRemoveUntil(
-          MaterialPageRoute(builder: (context) => const AddTodo()),
-          (Route<dynamic> route) => false);
-    });
-  }
-
-
-
-  // List<Todo> todos = [Todo(title: "vjnv",description: "vrjvv"),Todo(title: "dbvvb", description: "vudfdbvhvbf")];
-  // List<int> a = [1,4,6,7];
-  // List<String> b = ["cdhbcb","cdhbdcd","ihbhv"];
-
-
-  // _test(){
-  //   for(int i=0;i<todos.length;i++){
-  //     print(todos[i].title);
-  //     print(todos[i].description);
-  //   }
-  // }
-
-
-
-  @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      body: Container(
-        height: screenHeight,
-        width: screenWidth,
-         decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [darkBlueColor,lightBlueColor,whiteColor],
+      child: Scaffold(
+        body: Container(
+          height: screenHeight,
+          width: screenWidth,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [darkBlueColor, lightBlueColor, whiteColor],
+            ),
           ),
-        ),
-        child: Image.asset(
-          "assets/splash_image.png"
+          child: Image.asset("assets/splash_image.png"),
         ),
       ),
-     ),
     );
   }
 }
